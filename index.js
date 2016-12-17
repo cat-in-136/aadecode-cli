108129c2e1007cf40b614c20babac03709c14c06
cat_in_136 <cat.in.136+github@gmail.com>
2016-12-17 23:44:09 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+#!/bin/env node
+
+/* AADecode - Decode encoded-as-aaencode JavaScript program.
+ * 
+ * Copyright (C) 2010,2016 @cat_in_136
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+const fs = require("fs");
+const path = require("path");
+const minimist = require("minimist");
+
+const AADecode = {
+    decode: function(text) {
+        var evalPreamble = "(\uFF9F\u0414\uFF9F) ['_'] ( (\uFF9F\u0414\uFF9F) ['_'] (";
+        var decodePreamble = "( (\uFF9F\u0414\uFF9F) ['_'] (";
+        var evalPostamble = ") (\uFF9F\u0398\uFF9F)) ('_');";
+        var decodePostamble = ") ());";
+
+        // strip beginning/ending space.
+        text = text.replace(/^\s*/, "").replace(/\s*$/, "");
+
+        // returns empty text for empty input.
+        if (/^\s*$/.test(text)) {
+            return "";
+        }
+        // check if it is encoded.
+        if (text.lastIndexOf(evalPreamble) < 0) {
+            throw new Error("Given code is not encoded as aaencode.");
+        }
+        if (text.lastIndexOf(evalPostamble) != text.length - evalPostamble.length) {
+            throw new Error("Given code is not encoded as aaencode.");
+        }
+
+        var decodingScript = text.replace(evalPreamble, decodePreamble)
+                                 .replace(evalPostamble, decodePostamble);
+        return eval(decodingScript);
+    },
+    doDecode: function() {
+        var oEncoded = document.getElementById("aadecode_encoded");
+        var oDecoded = document.getElementById("aadecode_decoded");
+
+        try {
+            oDecoded.value = AADecode.decode(oEncoded.value);
+        } catch (ex) {
+            oDecoded.value = "****Error:\n" + ex.toString();
+        }
+    }
+};
+
+var argv = minimist(process.argv.slice(2), {
+    "string": ["output"],
+    "boolean": ["help", "version"],
+    "alias": {
+        h: "help",
+        o: "output",
+        v: "version"
+    }
+});
+
+if (argv.help) {
+    process.stdout.write("Usage: aadecode-cli [OPTION]... [FILE]\n");
+    process.stdout.write(" -o, --output=FILE    output to FILE\n");
+    process.stdout.write(" -h, --help           display this help and exit\n");
+    process.stdout.write(" -v, --version        output version information and exit\n");
+    process.exit(0);
+} else if (argv.version) {
+    const pkg = require("./package.json");
+    process.stdout.write(pkg.name + " " + pkg.version + "\n");
+    process.exit(0);
+} else {
+    (function () {
+        if (argv._.length == 0) {
+            return new Promise(function (resolve, reject) {
+                var text = "";
+                process.stdin.setEncoding("utf8");
+                process.stdin.on("readable", function () {
+                  text += process.stdin.read() || "";
+                });
+                process.stdin.on("end", function () {
+                  resolve(text);
+                });
+                process.stdin.on("error", function (error) {
+                  reject(error);
+                });
+            });
+        } else {
+            return new Promise(function (resolve, reject) {
+                fs.readFile(path.resolve(argv._[0]), function (error, text) {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(text.toString());
+                    }
+                });
+            });
+        }
+    })().then(function (text) {
+        if (argv.output) {
+            return new Promise(function (resolve, reject) {
+                fs.writeFile(argv.output, AADecode.decode(text), function (error) {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        } else {
+            process.stdout.write(AADecode.decode(text));
+            return Promise.resolve();
+        }
+    }).then(function () {
+        process.exit();
+    }).catch(function (error) {
+        console.error(error);
+        process.exit(1);
+    });
+}
