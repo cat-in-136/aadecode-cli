6fad2bf1cae60d8ed22633900ed5861bced4d03c
cat_in_136 <cat.in.136+github@gmail.com>
2016-12-18 00:39:15 +0900

    add test and split index.js to index.js and aadecode.js

--- index.js
+++ index.js
@@ -10,44 +10,7 @@
 const fs = require("fs");
 const path = require("path");
 const minimist = require("minimist");
-
-const AADecode = {
-    decode: function(text) {
-        var evalPreamble = "(\uFF9F\u0414\uFF9F) ['_'] ( (\uFF9F\u0414\uFF9F) ['_'] (";
-        var decodePreamble = "( (\uFF9F\u0414\uFF9F) ['_'] (";
-        var evalPostamble = ") (\uFF9F\u0398\uFF9F)) ('_');";
-        var decodePostamble = ") ());";
-
-        // strip beginning/ending space.
-        text = text.replace(/^\s*/, "").replace(/\s*$/, "");
-
-        // returns empty text for empty input.
-        if (/^\s*$/.test(text)) {
-            return "";
-        }
-        // check if it is encoded.
-        if (text.lastIndexOf(evalPreamble) < 0) {
-            throw new Error("Given code is not encoded as aaencode.");
-        }
-        if (text.lastIndexOf(evalPostamble) != text.length - evalPostamble.length) {
-            throw new Error("Given code is not encoded as aaencode.");
-        }
-
-        var decodingScript = text.replace(evalPreamble, decodePreamble)
-                                 .replace(evalPostamble, decodePostamble);
-        return eval(decodingScript);
-    },
-    doDecode: function() {
-        var oEncoded = document.getElementById("aadecode_encoded");
-        var oDecoded = document.getElementById("aadecode_decoded");
-
-        try {
-            oDecoded.value = AADecode.decode(oEncoded.value);
-        } catch (ex) {
-            oDecoded.value = "****Error:\n" + ex.toString();
-        }
-    }
-};
+const AADecode = require("./aadecode.js");
 
 var argv = minimist(process.argv.slice(2), {
     "string": ["output"],
